abcd94ce2f82e90db7bfb5c994a630df352f28e3
Anton Honcharov <4eyalush@gmail.com>
2016-08-09 23:21:20 +0300

    some reformat

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ scraper.scrape({
     urls: process.argv[2] !== undefined || process.argv[2] !== ''
         ? process.argv[2]
         : 'http://google.com',
-
     directory: process.argv[3]
 })
     .then(() => {console.log(`.then() :  ${process.argv}`)})
